63e118f50436c8c8aff8d7217cfb3c6733fb310f
felixbrunner <rubelrennfix@gmail.com>
2022-01-14 11:48:39 +0100

    update setup and requirements

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 from setuptools import find_packages, setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name="euraculus",
     version="0.1",
     description="FEVD networks for asset volatility spillovers",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="felixbrunner",
+    author_email="",
     packages=find_packages(),
     install_requires=[
         "numpy",
@@ -15,4 +21,10 @@ setup(
         "networkx",
         "glmnet-py",
     ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
 )
