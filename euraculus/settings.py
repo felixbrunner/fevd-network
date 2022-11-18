e605db74393538545c77fefa8a1017ba1c63d659
felixbrunner <rubelrennfix@gmail.com>
2022-11-18 18:00:07 -0500

    update settings

--- euraculus/settings.py
+++ euraculus/settings.py
@@ -6,28 +6,12 @@ import numpy as np
 
 # directories
 ROOT_DIR = Path(__file__).parents[1]
-DATA_DIR = ROOT_DIR / "data"
+DATA_DIR = ROOT_DIR / "data_new"
 
 # rolling window
-FIRST_SAMPLING_DATE = dt.datetime(year=1989, month=12, day=31)
+FIRST_SAMPLING_DATE = dt.datetime(year=1927, month=6, day=30)
 LAST_SAMPLING_DATE = dt.datetime(year=2022, month=3, day=31)
 TIME_STEP = relativedelta(months=1, day=31)
-
-# sampling
-NUM_ASSETS = 100
-SAMPLING_VARIABLE = "mcap_volatility"  # "mcap"
-ESTIMATION_WINDOW = 12
-FORECAST_WINDOW = 60
-
-# estimation
-FACTORS = ["crsp"]
-VAR_GRID = {
-    "alpha": np.geomspace(1e-4, 1e0, 13),
-    "lambdau": np.geomspace(1e-2, 1e1, 13),
-    #'gamma': np.geomspace(1e-2, 1e2, 15),
-}
-COV_GRID = {"alpha": np.geomspace(1e-3, 1e0, 25)}
-HORIZON = 21
 
 # analysis
 FORECAST_WINDOWS = [1, 2, 3, 6, 9, 12, 18, 24, 36, 48, 60]  # in months
@@ -37,6 +21,22 @@ FORECAST_WINDOWS = [1, 2, 3, 6, 9, 12, 18, 24, 36, 48, 60]  # in months
 #     ("fevd", weights),
 #     ("fev", weights),
 # ] # (tabe_name, weights??)
+
+# sampling
+NUM_ASSETS = 100
+SAMPLING_VARIABLE = "mcap_volatility"  # "mcap"
+ESTIMATION_WINDOW = 12
+FORECAST_WINDOW = max(FORECAST_WINDOWS)
+
+# estimation
+FACTORS = ["crsp_log_vola"]
+VAR_GRID = {
+    "alpha": np.geomspace(1e-4, 1e0, 13),
+    "lambdau": np.geomspace(1e-2, 1e1, 13),
+    #'gamma': np.geomspace(1e-2, 1e2, 15),
+}
+COV_GRID = {"alpha": np.geomspace(1e-3, 1e0, 50)}
+HORIZON = 21
 
 # colors for plotting
 DARK_BLUE = "#014c63"
