f0d29828dcd7dcba068d4fd15ed26087dc368a12
felixbrunner <rubelrennfix@gmail.com>
2022-11-18 19:35:21 -0500

    improve aggregates notebook

--- notebooks/0_data/02_collect_aggregates.py
+++ notebooks/0_data/02_collect_aggregates.py
@@ -26,12 +26,13 @@ from euraculus.data.preprocess import construct_index, count_obs, construct_norm
 
 data = DataMap(DATA_DIR)
 
+df_rf = data.load_rf()
 df_spy = data.load_spy_data()
 df_vix = data.load_yahoo("^VIX")
 df_dxy = data.load_yahoo("DX-Y.NYB")
 df_tnx = data.load_yahoo("^TNX")
 
-# ## Extract
+# ## Extract historic indices
 
 # %%time
 # extract aggregate information for each sample
@@ -40,50 +41,112 @@ while sampling_date <= LAST_SAMPLING_DATE:
     # load & set up
     df_historic = data.load_historic(sampling_date=sampling_date)
     index = df_historic.iloc[:, 0].unstack().index
+    rf = df_rf.reindex(index).values.squeeze()
     df_crsp = data.load_crsp_data(
         start_date=df_historic.index.get_level_values("date")[0],
         end_date=df_historic.index.get_level_values("date")[-1],
     )
-    df_aggregates = pd.DataFrame(index=index)
+    df_historic_aggregates = pd.DataFrame(index=index)
     
     # simple indices
-    df_aggregates["crsp_ew"] = construct_index(df_crsp, column="retadj", weighting_column=None, logs=False)
-    df_aggregates["crsp_vw"] = construct_index(df_crsp, column="retadj", weighting_column="mcap", logs=False)
-    df_aggregates["crsp_var_ew"] = construct_index(df_crsp, column="var", weighting_column=None, logs=False)
-    df_aggregates["crsp_var_vw"] = construct_index(df_crsp, column="var", weighting_column="mcap", logs=False)
-    df_aggregates["sample_ew"] = construct_index(df_historic, column="retadj", weighting_column=None, logs=False)
-    df_aggregates["sample_vw"] = construct_index(df_historic, column="retadj", weighting_column="mcap", logs=False)
-    df_aggregates["sample_var_ew"] = construct_index(df_historic, column="var", weighting_column=None, logs=False)
-    df_aggregates["sample_var_vw"] = construct_index(df_historic, column="var", weighting_column="mcap", logs=False)
+    df_historic_aggregates["crsp_ew"] = construct_index(df_crsp, column="retadj", weighting_column=None, logs=False).sub(rf)
+    df_historic_aggregates["crsp_vw"] = construct_index(df_crsp, column="retadj", weighting_column="mcap", logs=False).sub(rf)
+    df_historic_aggregates["crsp_var_ew"] = construct_index(df_crsp, column="var", weighting_column=None, logs=False)
+    df_historic_aggregates["crsp_var_vw"] = construct_index(df_crsp, column="var", weighting_column="mcap", logs=False)
+    df_historic_aggregates["sample_ew"] = construct_index(df_historic, column="retadj", weighting_column=None, logs=False).sub(rf)
+    df_historic_aggregates["sample_vw"] = construct_index(df_historic, column="retadj", weighting_column="mcap", logs=False).sub(rf)
+    df_historic_aggregates["sample_var_ew"] = construct_index(df_historic, column="var", weighting_column=None, logs=False)
+    df_historic_aggregates["sample_var_vw"] = construct_index(df_historic, column="var", weighting_column="mcap", logs=False)
     
     # observation counts
-    df_aggregates["crsp_num_ret"] = count_obs(df_crsp, column="retadj")
-    df_aggregates["crsp_num_var"] = count_obs(df_crsp, column="var")
-    df_aggregates["crsp_num_noisevar"] = count_obs(df_crsp, column="noisevar")
-    df_aggregates["sample_num_ret"] = count_obs(df_historic, column="retadj")
-    df_aggregates["sample_num_var"] = count_obs(df_historic, column="var")
-    df_aggregates["sample_num_noisevar"] = count_obs(df_historic, column="noisevar")
+    df_historic_aggregates["crsp_num_ret"] = count_obs(df_crsp, column="retadj")
+    df_historic_aggregates["crsp_num_var"] = count_obs(df_crsp, column="var")
+    df_historic_aggregates["crsp_num_noisevar"] = count_obs(df_crsp, column="noisevar")
+    df_historic_aggregates["sample_num_ret"] = count_obs(df_historic, column="retadj")
+    df_historic_aggregates["sample_num_var"] = count_obs(df_historic, column="var")
+    df_historic_aggregates["sample_num_noisevar"] = count_obs(df_historic, column="noisevar")
     
     # normalized indices
-    df_aggregates["crsp_vola"] = construct_normalized_vola_index(df_crsp, logs=False).rename("crsp_vola")
-    df_aggregates["crsp_log_vola"] = construct_normalized_vola_index(df_crsp, logs=True).rename("crsp_log_vola")
-    df_aggregates["sample_vola"] = construct_normalized_vola_index(df_historic, logs=False).rename("sample_vola")
-    df_aggregates["sample_log_vola"] = construct_normalized_vola_index(df_historic, logs=True).rename("sample_log_vola")
+    df_historic_aggregates["crsp_vola"] = construct_normalized_vola_index(df_crsp, logs=False)
+    df_historic_aggregates["crsp_log_vola"] = construct_normalized_vola_index(df_crsp, logs=True)
+    df_historic_aggregates["sample_vola"] = construct_normalized_vola_index(df_historic, logs=False)
+    df_historic_aggregates["sample_log_vola"] = construct_normalized_vola_index(df_historic, logs=True)
     
     # spy
-    df_aggregates["spy_ret"] = df_spy.reindex(index)["ret"]
-    df_aggregates["spy_vola"] = np.sqrt(df_spy.reindex(index)["var"])
-    df_aggregates["vix_ret"] = df_vix.reindex(index)["ret"]
-    df_aggregates["vix_vola"] = np.sqrt(df_vix.reindex(index)["var"])
-    df_aggregates["dxy_ret"] = df_dxy.reindex(index)["ret"]
-    df_aggregates["dxy_vola"] = np.sqrt(df_dxy.reindex(index)["var"])
-    df_aggregates["tnx_ret"] = df_tnx.reindex(index)["ret"]
-    df_aggregates["tnx_vola"] = np.sqrt(df_tnx.reindex(index)["var"])
+    df_historic_aggregates["spy_ret"] = df_spy.reindex(index)["ret"].sub(rf)
+    df_historic_aggregates["spy_vola"] = np.sqrt(df_spy.reindex(index)["var"])
+    df_historic_aggregates["vix_ret"] = df_vix.reindex(index)["ret"].sub(rf)
+    df_historic_aggregates["vix_vola"] = np.sqrt(df_vix.reindex(index)["var"])
+    df_historic_aggregates["dxy_ret"] = df_dxy.reindex(index)["ret"].sub(rf)
+    df_historic_aggregates["dxy_vola"] = np.sqrt(df_dxy.reindex(index)["var"])
+    df_historic_aggregates["tnx_ret"] = df_tnx.reindex(index)["ret"].sub(rf)
+    df_historic_aggregates["tnx_vola"] = np.sqrt(df_tnx.reindex(index)["var"])
     
     # store
     data.store(
-        df_aggregates,
-        f"samples/{sampling_date:%Y-%m-%d}/aggregates.csv",
+        df_historic_aggregates,
+        f"samples/{sampling_date:%Y-%m-%d}/historic_aggregates.csv",
+    )
+
+    # increment monthly end of month
+    if sampling_date.month == 12:
+        print(f"Done sampling year {sampling_date.year}.")
+    sampling_date += TIME_STEP
+
+# ## Extract future indices
+
+# %%time
+# extract aggregate information for each sample
+sampling_date = FIRST_SAMPLING_DATE
+while sampling_date <= LAST_SAMPLING_DATE:
+    # load & set up
+    df_future = data.load_future(sampling_date=sampling_date)
+    index = df_future.iloc[:, 0].unstack().index
+    rf = df_rf.reindex(index).values.squeeze()
+    df_crsp = data.load_crsp_data(
+        start_date=df_future.index.get_level_values("date")[0],
+        end_date=df_future.index.get_level_values("date")[-1],
+    )
+    df_future_aggregates = pd.DataFrame(index=index)
+    
+    # simple indices
+    df_future_aggregates["crsp_ew"] = construct_index(df_crsp, column="retadj", weighting_column=None, logs=False).sub(rf)
+    df_future_aggregates["crsp_vw"] = construct_index(df_crsp, column="retadj", weighting_column="mcap", logs=False).sub(rf)
+    df_future_aggregates["crsp_var_ew"] = construct_index(df_crsp, column="var", weighting_column=None, logs=False)
+    df_future_aggregates["crsp_var_vw"] = construct_index(df_crsp, column="var", weighting_column="mcap", logs=False)
+    df_future_aggregates["sample_ew"] = construct_index(df_future, column="retadj", weighting_column=None, logs=False).sub(rf)
+    df_future_aggregates["sample_vw"] = construct_index(df_future, column="retadj", weighting_column="mcap", logs=False).sub(rf)
+    df_future_aggregates["sample_var_ew"] = construct_index(df_future, column="var", weighting_column=None, logs=False)
+    df_future_aggregates["sample_var_vw"] = construct_index(df_future, column="var", weighting_column="mcap", logs=False)
+    
+    # observation counts
+    df_future_aggregates["crsp_num_ret"] = count_obs(df_crsp, column="retadj")
+    df_future_aggregates["crsp_num_var"] = count_obs(df_crsp, column="var")
+    df_future_aggregates["crsp_num_noisevar"] = count_obs(df_crsp, column="noisevar")
+    df_future_aggregates["sample_num_ret"] = count_obs(df_future, column="retadj")
+    df_future_aggregates["sample_num_var"] = count_obs(df_future, column="var")
+    df_future_aggregates["sample_num_noisevar"] = count_obs(df_future, column="noisevar")
+    
+    # normalized indices
+    df_future_aggregates["crsp_vola"] = construct_normalized_vola_index(df_crsp, logs=False).rename("crsp_vola")
+    df_future_aggregates["crsp_log_vola"] = construct_normalized_vola_index(df_crsp, logs=True).rename("crsp_log_vola")
+    df_future_aggregates["sample_vola"] = construct_normalized_vola_index(df_future, logs=False).rename("sample_vola")
+    df_future_aggregates["sample_log_vola"] = construct_normalized_vola_index(df_future, logs=True).rename("sample_log_vola")
+    
+    # spy
+    df_future_aggregates["spy_ret"] = df_spy.reindex(index)["ret"].sub(rf)
+    df_future_aggregates["spy_vola"] = np.sqrt(df_spy.reindex(index)["var"])
+    df_future_aggregates["vix_ret"] = df_vix.reindex(index)["ret"].sub(rf)
+    df_future_aggregates["vix_vola"] = np.sqrt(df_vix.reindex(index)["var"])
+    df_future_aggregates["dxy_ret"] = df_dxy.reindex(index)["ret"].sub(rf)
+    df_future_aggregates["dxy_vola"] = np.sqrt(df_dxy.reindex(index)["var"])
+    df_future_aggregates["tnx_ret"] = df_tnx.reindex(index)["ret"].sub(rf)
+    df_future_aggregates["tnx_vola"] = np.sqrt(df_tnx.reindex(index)["var"])
+    
+    # store
+    data.store(
+        df_future_aggregates,
+        f"samples/{sampling_date:%Y-%m-%d}/future_aggregates.csv",
     )
 
     # increment monthly end of month
